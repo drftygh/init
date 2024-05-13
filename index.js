7e637cc5a85acaebcbfe5868ff9db040902b7d5f
drftygh <nsjcathanmontez@hotmail.com>
2024-05-13 18:40:49 +0700

    demo project

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const firstName = "John";
-const lastName = "Doe";
-const fullName = firstName + " " + lastName;
-console.log(fullName); // John Doe
+function mergeKLists(lists) {
+  if (lists.length === 0) return null;
+  while (lists.length > 1) {
+    const first = lists.shift();
+    const second = lists.shift();
+    const merged = mergeTwoLists(first, second);
+    lists.push(merged);
+  }
+  return lists[0];
+}
